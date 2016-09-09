9e518be857ac76a3527dfafef0dc9724d226b7e0
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-09-08 20:57:45 -0500

    allow api to do a backup search if address yeilds no results

--- app/models/chi_api.rb
+++ app/models/chi_api.rb
@@ -8,11 +8,11 @@ class ChiApi
     @base_uri="https://data.cityofchicago.org/resource/cwig-ma7x.json?"
   end
 
-  def find_business(address)
+  def find_business(address, name, zipcode)
     biz_address = format_address(address)
-    #find resturants match address, sort by latest inspection and return that report
-    response = HTTParty.get("#{@base_uri}address=#{biz_address}&city=CHICAGO&$$app_token=#{ENV['CHI_TOKEN']}&$order=inspection_date DESC")
-    response.parsed_response[0]
+    #find resturants match address (if address result in nother, try finding by name & zipcode), sort by latest inspection and return that report
+    response = HTTParty.get("#{@base_uri}address=#{biz_address}&city=CHICAGO&$$app_token=#{ENV['CHI_TOKEN']}&$order=inspection_date DESC").parsed_response[0] ||=  HTTParty.get("#{@base_uri}aka_name=#{name.upcase}&zip=#{zipcode}&$$app_token=#{ENV['CHI_TOKEN']}&$order=inspection_date DESC").parsed_response[0]
+    p response
   end
 
   def format_address(original_address)
