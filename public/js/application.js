482b2b057726cca264490fb814d3f2d72766ec75
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-09-08 21:29:27 -0500

    ajax search results

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $("#search-form").on("submit", function(e){
+    e.preventDefault();
+    $("#search-status").text("Searching")
+    $form = $(this)
+    data = $(this).serialize();
+    console.log(data);
+    $.get( "/searches", data )
+      .done(function( response ) {
+        $("#search-status").text("Results:")
+        $("#search-list").append(response).show('slow');
+      });
+    });
 });
