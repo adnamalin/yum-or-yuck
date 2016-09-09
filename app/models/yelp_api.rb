686f6ce72326d2b74e50accae55d2987566f07e7
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-09-08 19:03:06 -0500

    yelp client created

--- app/models/yelp_api.rb
+++ app/models/yelp_api.rb
@@ -1,16 +1,23 @@
-require 'httparty'
-require 'SecureRandom'
+require 'yelp'
+require 'dotenv'
+Dotenv.load
 
 class YelpApi
 
   def initialize
-    @base_url = "https://api.yelp.com/v2/search?"
+    @client = Yelp::Client.new({ consumer_key: ENV['YELP_CONSUMER_KEY'],
+                            consumer_secret: ENV['YELP_CONSUMER_SECRET'],
+                            token: ENV['YELP_TOKEN'],
+                            token_secret: ENV['YELP_TOKEN_SECRET']
+                          })
   end
 
   def search_restaurants(term)
-
+    results = @client.search('Chicago', {term: term})
+    p results
   end
 
 end
 
 yelp = YelpApi.new
+yelp.search_restaurants('korean')
