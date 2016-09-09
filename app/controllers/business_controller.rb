240ad239866942a800520aabf4755a4642aad127
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-09-08 21:06:01 -0500

    clean up p statements

--- app/controllers/business_controller.rb
+++ app/controllers/business_controller.rb
@@ -1,10 +1,7 @@
 get '/business/:term' do
   yelp = YelpApi.new
   chi_api = ChiApi.new
-  p params[:term]
   @biz = yelp.select_business(params[:term])
-  p @biz
   @inspection_report = chi_api.find_business(@biz.location.address[0], @biz.name, @biz.location.postal_code)
-  p @inspection_report
   erb :'businesses/show'
 end
