aa3fd832148c4363d5b8243d4ba57084db40c742
Amanda Lin <adnama.lin@gmail.com>
2016-09-09 21:06:53 -0500

    Update business_controller.rb

--- app/controllers/business_controller.rb
+++ app/controllers/business_controller.rb
@@ -3,6 +3,5 @@ get '/business/:term' do
   chi_api = ChiApi.new
   @biz = yelp.select_business(params[:term])
   @inspection_report = chi_api.find_business(@biz.location.address[0], @biz.name, @biz.location.postal_code)
-  p @inspection_report
   erb :'businesses/show', locals: {biz: @biz, inspection_report: @inspection_report}
 end
